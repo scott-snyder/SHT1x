737620e75dac4beda50cc598f18551dcbc9e3beb
Vincent Pang <augostino@gmail.com>
2017-08-27 19:42:04 +0800

    updated coefficients, added voltage option, code cleanup

--- examples/ReadSHT1xValues/ReadSHT1xValues.cpp
+++ examples/ReadSHT1xValues/ReadSHT1xValues.cpp
@@ -8,17 +8,20 @@
  * www.practicalarduino.com
  */
 
+#include <Arduino.h>
+
 #include <SHT1x.h>
 
 // Specify data and clock connections and instantiate SHT1x object
 #define dataPin  10
-#define clockPin 11
+#define clockPin 13
+
 SHT1x sht1x(dataPin, clockPin);
 
 void setup()
 {
-   Serial.begin(38400); // Open serial connection to report values to host
-   Serial.println("Starting up");
+  Serial.begin(9600); // Open serial connection to report values to host
+  Serial.println("Starting up");
 }
 
 void loop()
