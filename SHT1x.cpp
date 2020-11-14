164a854f5bf9f72f0d154e19d297026e9615f727
scott snyder <sss@karma>
2020-11-14 18:33:10 -0500

    Refresh of fixes

--- SHT1x.cpp
+++ SHT1x.cpp
@@ -33,14 +33,15 @@ SHT1x::SHT1x(uint8_t dataPin, uint8_t clockPin, Voltage voltage) :
 /**
  * Reads the current temperature in degrees Celsius
  */
-float SHT1x::readTemperatureC() const
+float SHT1x::readTemperatureC(int* err /*=nullptr*/) const
 {
   // Conversion coefficients from datasheet
   const double D1 = getD1ForC(_voltage);
   const double D2 = getD2ForC(_tempResolution);
 
   // Fetch raw value
-  uint16_t rawData = readRawData(ShtCommand::MeasureTemperature, _dataPin, _clockPin);
+  if (err) *err = 0;
+  uint16_t rawData = readRawData(ShtCommand::MeasureTemperature, err);
 
   // Convert raw value to degrees Celsius
   float temperature = D1 + D2 * rawData;
@@ -51,14 +52,15 @@ float SHT1x::readTemperatureC() const
 /**
  * Reads the current temperature in degrees Fahrenheit
  */
-float SHT1x::readTemperatureF() const
+float SHT1x::readTemperatureF(int* err /*=nullptr*/) const
 {
   // Conversion coefficients from datasheet
   const double D1 = getD1ForF(_voltage);
   const double D2 = getD2ForF(_tempResolution);
 
   // Fetch raw value
-  uint16_t rawData = readRawData(ShtCommand::MeasureTemperature, _dataPin, _clockPin);
+  if (err) *err = 0;
+  uint16_t rawData = readRawData(ShtCommand::MeasureTemperature, err);
 
   // Convert raw value to degrees Fahrenheit
   float temperature = D1 + D2 * rawData;
@@ -69,7 +71,7 @@ float SHT1x::readTemperatureF() const
 /**
  * Reads current temperature-corrected relative humidity
  */
-float SHT1x::readHumidity() const
+float SHT1x::readHumidity (int* err /*= nullptr*/) const
 {
   // Conversion coefficients from datasheet
   const double C1 = getC1(_humidityResolution);
@@ -79,13 +81,16 @@ float SHT1x::readHumidity() const
   const double T2 = getT2(_humidityResolution);
 
   // Fetch the value from the sensor
-  uint16_t rawData = readRawData(ShtCommand::MeasureRelativeHumidity, _dataPin, _clockPin);
+  if (err) *err = 0;
+  uint16_t rawData = readRawData(ShtCommand::MeasureRelativeHumidity, err);
 
   // Apply linear conversion to raw value
   double linearHumidity = C1 + C2 * rawData + C3 * rawData * rawData;
 
   // Get current temperature for humidity correction
-  float temperature = readTemperatureC();
+  int err2 = 0;
+  float temperature = readTemperatureC(&err2);
+  if (err && err2) *err = err2;
 
   // Correct humidity value for current temperature
   float correctedHumidity = (temperature - 25.0) * (T1 + T2 * rawData) + linearHumidity;
@@ -99,48 +104,55 @@ float SHT1x::readHumidity() const
 /**
  * Reads the raw data from sensor
  */
-uint16_t SHT1x::readRawData(ShtCommand command, uint8_t dataPin, uint8_t clockPin) const
+uint16_t SHT1x::readRawData(ShtCommand command, int* err) const
 {
-  sendCommandSHT(command, dataPin, clockPin);
-  waitForResultSHT(dataPin);
-  uint16_t result = getData16SHT(dataPin, clockPin);
-  skipCrcSHT(dataPin, clockPin);
+  sendCommandSHT(command, err);
+  waitForResultSHT(err);
+  uint16_t result = getData16SHT(err);
+  uint8_t crccalc = calcCRC (command, 0, result, 2);
+  uint8_t crc = readCrcSHT();
+  if (err && crc != crccalc) {
+    *err = 4;
+  }
 
   return result;
 }
 
 /**
  */
-bool SHT1x::sendCommandSHT(ShtCommand command, uint8_t dataPin, uint8_t clockPin) const
+bool SHT1x::sendCommandSHT(ShtCommand command, int* err) const
 {
   // Transmission Start
-  pinMode(dataPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  digitalWrite(dataPin, HIGH);
-  digitalWrite(clockPin, HIGH);
-  digitalWrite(dataPin, LOW);
-  digitalWrite(clockPin, LOW);
-  digitalWrite(clockPin, HIGH);
-  digitalWrite(dataPin, HIGH);
-  digitalWrite(clockPin, LOW);
+  pinMode(_dataPin, OUTPUT);
+  pinMode(_clockPin, OUTPUT);
+  digitalWrite(_dataPin, HIGH);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_dataPin, LOW);
+  digitalWrite(_clockPin, LOW);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_dataPin, HIGH);
+  digitalWrite(_clockPin, LOW);
 
   // The command (3 msb are address and must be 000, and last 5 bits are command)
-  shiftOut(dataPin, clockPin, MSBFIRST, static_cast<uint8_t>(command));
+  shiftOut(_dataPin, _clockPin, MSBFIRST, static_cast<uint8_t>(command));
 
   // Verify we get the correct ack
-  digitalWrite(clockPin, HIGH);
-  pinMode(dataPin, INPUT);
-  int ack = digitalRead(dataPin);
+  digitalWrite(_clockPin, HIGH);
+  pinMode(_dataPin, INPUT);
+  int ack = digitalRead(_dataPin);
   if (ack != LOW)
   {
     // Serial.println("Ack Error 0");
+    if (err) *err = 1;
     return false;
   }
-  digitalWrite(clockPin, LOW);
-  ack = digitalRead(dataPin);
+  digitalWrite(_clockPin, LOW);
+  delay(1);
+  ack = digitalRead(_dataPin);
   if (ack != HIGH)
   {
     // Serial.println("Ack Error 1");
+    if (err) *err = 2;
     return false;
   }
 
@@ -149,9 +161,9 @@ bool SHT1x::sendCommandSHT(ShtCommand command, uint8_t dataPin, uint8_t clockPin
 
 /**
  */
-bool SHT1x::waitForResultSHT(uint8_t dataPin) const
+bool SHT1x::waitForResultSHT(int* err) const
 {
-  pinMode(dataPin, INPUT);
+  pinMode(_dataPin, INPUT);
 
   // wait for at most 1000ms for the result
   // from the datasheet,
@@ -161,7 +173,7 @@ bool SHT1x::waitForResultSHT(uint8_t dataPin) const
   for(uint8_t i= 0; i < 100; ++i)
   {
     delay(10);
-    ack = digitalRead(dataPin);
+    ack = digitalRead(_dataPin);
 
     if (ack == LOW)
     {
@@ -172,32 +184,33 @@ bool SHT1x::waitForResultSHT(uint8_t dataPin) const
   if (ack == HIGH)
   {
     // Serial.println("Ack Error 2"); // Can't do serial stuff here, need another way of reporting errors
+    if (err) *err = 3;
     return false;
   }
 
   return true;
 }
 
-uint16_t SHT1x::getData16SHT(uint8_t dataPin, uint8_t clockPin) const
+uint16_t SHT1x::getData16SHT(int* err) const
 {
   uint8_t rawData[2];
   memset(rawData, 0, 2);
 
   // Get the most significant bits
-  pinMode(dataPin, INPUT);
-  rawData[0] = shiftIn(dataPin, clockPin, MSBFIRST);
+  pinMode(_dataPin, INPUT);
+  rawData[0] = this->shiftIn();
 
   // Send the required ack
-  pinMode(dataPin, OUTPUT);
-  digitalWrite(dataPin, LOW);
+  pinMode(_dataPin, OUTPUT);
+  digitalWrite(_dataPin, LOW);
 
-  pinMode(clockPin, OUTPUT);
-  digitalWrite(clockPin, HIGH);
-  digitalWrite(clockPin, LOW);
+  pinMode(_clockPin, OUTPUT);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_clockPin, LOW);
 
   // Get the least significant bits
-  pinMode(dataPin, INPUT);
-  rawData[1] = shiftIn(dataPin, clockPin, MSBFIRST);
+  pinMode(_dataPin, INPUT);
+  rawData[1] = this->shiftIn();
 
   // extract value from the big-endian raw data
   uint16_t result = (rawData[0] << 8) | (rawData[1] << 0);
@@ -207,15 +220,62 @@ uint16_t SHT1x::getData16SHT(uint8_t dataPin, uint8_t clockPin) const
 
 /**
  */
-void SHT1x::skipCrcSHT(uint8_t dataPin, uint8_t clockPin) const
+void SHT1x::skipCrcSHT() const
 {
   // Skip acknowledge to end trans (no CRC)
-  pinMode(dataPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
+  pinMode(_dataPin, OUTPUT);
+  pinMode(_clockPin, OUTPUT);
 
-  digitalWrite(dataPin, HIGH);
-  digitalWrite(clockPin, HIGH);
-  digitalWrite(clockPin, LOW);
+  digitalWrite(_dataPin, HIGH);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_clockPin, LOW);
+}
+
+/**
+ */
+uint8_t SHT1x::readCrcSHT() const
+{
+  pinMode(_dataPin, OUTPUT);
+  digitalWrite(_dataPin, LOW);
+
+  pinMode(_clockPin, OUTPUT);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_clockPin, LOW);
+    
+  uint8_t crc = this->shiftIn();
+    
+  pinMode(_dataPin, OUTPUT);
+  pinMode(_clockPin, OUTPUT);
+
+  digitalWrite(_dataPin, HIGH);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_clockPin, LOW);
+
+  return crc;
+}
+
+uint8_t SHT1x::crc8(const uint8_t data, const uint8_t startval) const
+{
+  /* Algorithm based on the Sensirion CRC8 calculation app note
+   * Note! variable crc is reversed compared to the app note
+   */
+  uint8_t crc = startval;
+
+  for (int i = 7; i >= 0; i--) {
+    crc = (((data >> i) & 1) == (crc & 1)) ? crc >> 1 : ((crc >> 1) ^ 0xC) | 0x80;
+  }
+  return crc;
+}
+
+uint8_t SHT1x::calcCRC(ShtCommand command, uint8_t status, uint16_t data,
+                       unsigned nbytes) const
+{
+  uint8_t crc = crc8(static_cast<uint8_t>(command), 0);
+  if (nbytes > 1) {
+    crc = crc8 ((data>>8)&0xff, crc);
+  }
+  crc = crc8 (data&0xff, crc);
+  return crc;
 }
 
 double SHT1x::getC1(SHT1x::HumidityMeasurementResolution resolution) const
@@ -314,4 +374,20 @@ double SHT1x::getD2ForF(SHT1x::TemperatureMeasurementResolution resolution) cons
                           };
 
   return VALUES[static_cast<uint8_t>(resolution)];
+}
+
+
+uint8_t SHT1x::shiftIn() const
+{
+  pinMode(_dataPin, INPUT);
+  pinMode(_clockPin, OUTPUT);
+
+  unsigned ret = 0;
+  for (unsigned i = 0; i < 8; i++) {
+    digitalWrite (_clockPin, HIGH);
+    delay(1);
+    ret = (ret<<1) + digitalRead (_dataPin);
+    digitalWrite (_clockPin, LOW);
+  }
+  return ret;
 }
